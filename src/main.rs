06966bcaa9f9d7597e82fc3a099e7b4f6e29b68d
Nisse Knudsen <nisse.knudsen@gmail.com>
2025-08-13 11:24:50 -0700

    Rerun integration

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
-use make87::interfaces::zenoh::{ZenohInterface};
-// use rerun;
+use make87::encodings::{Encoder, ProtobufEncoder};
+use make87::interfaces::zenoh::ZenohInterface;
+use make87::interfaces::zenoh::ZenohInterfaceError::SubTopicNotFound;
+use make87_messages::text::PlainText;
 use std::error::Error;
 
 #[tokio::main]
@@ -8,50 +10,44 @@ async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
 
     let application_config = make87::config::load_config_from_default_env()?;
 
-    // let message_encoder = ProtobufEncoder::<PlainText>::new();
+    let message_encoder = ProtobufEncoder::<PlainText>::new();
     let zenoh_interface = ZenohInterface::new(application_config.clone(), "zenoh");
     let session = zenoh_interface.get_session().await?;
-    // let subscriber_config = zenoh_interface.get_subscriber_config("any_message").ok_or_else(|| SubTopicNotFound("any_message".to_string()))?;
+    let subscriber_config = zenoh_interface
+        .get_subscriber_config("any_message")
+        .ok_or_else(|| SubTopicNotFound("any_message".to_string()))?;
 
+    let rerun_grpc_client = application_config
+        .interfaces
+        .get("rerun-grpc")
+        .ok_or_else(|| "Interface 'rerun-grpc' not found.")?
+        .clients
+        .get("rerun-grpc-client")
+        .ok_or_else(|| "Client 'rerun-grpc-client' not found.")?;
 
-    // let rerun_grpc_client = application_config.interfaces.get("rerun-grpc").ok_or_else(|| "Interface 'rerun-grpc' not found.")?.clients.get("rerun-grpc-client").ok_or_else(|| "Client 'rerun-grpc-client' not found.")?;
-    //
-    // let rec = rerun::RecordingStreamBuilder::new("make87_messages_shipper")
-    //     .connect_grpc_opts(format!("rerun+http://{}:{}/proxy", rerun_grpc_client.access_point.vpn_ip, rerun_grpc_client.access_point.vpn_port), rerun::default_flush_timeout())?;
+    let rec = rerun::RecordingStreamBuilder::new("make87_messages_shipper").connect_grpc_opts(
+        format!(
+            "rerun+http://{}:{}/proxy",
+            rerun_grpc_client.access_point.vpn_ip, rerun_grpc_client.access_point.vpn_port
+        ),
+        rerun::default_flush_timeout(),
+    )?;
 
-    let key_expr = "*/*/make87_messages-$*/**".to_string();
-
-    println!("Declaring Subscriber on '{}'...", &key_expr);
-    let subscriber = session.declare_subscriber(&key_expr).await?;
-
-    println!("Press CTRL-C to quit...");
-    while let Ok(sample) = subscriber.recv_async().await {
-        log::info!(
-            ">> [Subscriber] Received {} ('{}')",
-            sample.kind(),
-            sample.key_expr().as_str(),
-        );
-        if let Some(att) = sample.attachment() {
-            let att = att.try_to_string().unwrap_or_else(|e| e.to_string().into());
-            print!(" ({att})");
-        }
-    }
-
-
-    // let subscriber = session
-    //     .declare_subscriber("*/*/make87_messages-$*/**")
-    //     .callback(move |sample| {
-    //         log::info!("Received sample. Topic: {}", sample.key_expr());
-    //         // let message_decoded = message_encoder.decode(&sample.payload().to_bytes());
-    //         // match message_decoded {
-    //         //     Ok(msg) => {
-    //         //         log::info!("Received message: {:?}", msg);
-    //         //         rec.log("any_message", &msg).unwrap();
-    //         //     },
-    //         //     Err(e) => log::error!("Decode error: {}", e),
-    //         // }
-    //     })
-    //     .await?;
+    session
+        .declare_subscriber(&subscriber_config.config.topic_key)
+        .callback(move |sample| {
+            log::info!("Received sample. Topic: {}", sample.key_expr());
+            let message_decoded = message_encoder.decode(&sample.payload().to_bytes());
+            match message_decoded {
+                Ok(msg) => {
+                    log::info!("Received message: {:?}", msg);
+                    rec.log("/any_message", &rerun::TextDocument::new(msg.body))
+                        .unwrap();
+                }
+                Err(e) => log::error!("Decode error: {}", e),
+            }
+        })
+        .await?;
 
     make87::run_forever();
 
