a7137157e2a261b9118af618aa473b771671a9e6
Nisse Knudsen <nisse.knudsen@gmail.com>
2025-09-04 11:16:54 -0700

    added subscriber print statements

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
+use make87::interfaces::rerun::RerunGRpcInterface;
 use make87::interfaces::zenoh::ZenohInterface;
 use make87::interfaces::zenoh::ZenohInterfaceError::SubTopicNotFound;
 use std::error::Error;
-use make87::interfaces::rerun::RerunGRpcInterface;
 
 mod message_handlers;
 use message_handlers::MessageTypeRegistry;
@@ -18,17 +18,25 @@ async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
         .get_subscriber_config("any_message")
         .ok_or_else(|| SubTopicNotFound("any_message".to_string()))?;
 
-    let rerun_grpc_interface = RerunGRpcInterface::new(application_config.clone(),"rerun-grpc");
+    let rerun_grpc_interface = RerunGRpcInterface::new(application_config.clone(), "rerun-grpc");
     let rec = rerun_grpc_interface.get_client_recording_stream("rerun-grpc-client")?;
 
     // Create registry and determine handler from topic_key
     let registry = MessageTypeRegistry::new();
-    let handler = registry.create_handler_from_topic_key(&subscriber_config.config.topic_key)
-        .ok_or_else(|| format!("Unknown message type for topic: {}", subscriber_config.config.topic_key))?;
+    let handler = registry
+        .create_handler_from_topic_key(&subscriber_config.config.topic_key)
+        .ok_or_else(|| {
+            format!(
+                "Unknown message type for topic: {}",
+                subscriber_config.config.topic_key
+            )
+        })?;
 
-    let subscriber = session.declare_subscriber(&subscriber_config.config.topic_key).await?;
+    let subscriber = session
+        .declare_subscriber(&subscriber_config.config.topic_key)
+        .await?;
     while let Ok(sample) = subscriber.recv_async().await {
-        log::info!("Received sample. Topic: {}", sample.key_expr());
+        println!("Received sample. Topic: {}", sample.key_expr());
 
         if let Err(e) = handler.handle_message(&sample, &rec) {
             log::error!("Error handling message: {}", e);
